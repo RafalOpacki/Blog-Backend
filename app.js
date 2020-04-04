598e4760d6015f643d860c0ef8b9a2ba8ab2f62c
Rafal Opacki <rafal.opacki@jit.team>
2020-04-05 01:30:57 +0200

    Update env

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ require('dotenv').config();
 const app = express();
 const port = 3000;
 
-app.listen(port, () => console.log(`Server is live on port ${port}`));
+app.listen(process.env.PORT || port, () =>
+  console.log(`Server is live on port ${process.env.PORT || port}`),
+);
 
 mongoose.connect(
   process.env.DB_CONNECTION_STRING,
